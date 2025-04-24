830e4e8b7c2adca800510f45dc0f511ad87e517a
fennydev <fenilmehta7077@gmail.com>
2025-04-24 19:29:11 +1000

    cleanup

---
+++ portfolio/src/components/header.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { useState } from 'react'
+import { useEffect } from 'react'
+import { Home, User, Book } from 'lucide-react'
+
+
+
+const Header = () => {
+    return (
+        <div id='header-main' className='flex flex-col sm:flex-row items-center justify-between p-4 bg-gray-800 text-white'>
+            <div className='flex items-center gap-4'>
+                
+            </div>
+            
+        </div>
+    )
+}
+
+export default Header
